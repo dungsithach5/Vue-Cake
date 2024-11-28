54263a0062047c148b2e688d6d2fcf304445a0c6
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2024-11-29 01:12:36 +0700

    update file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,12 @@
 				<div class="fullwidthbanner">
 					<div class="bannercontainer" >
 					<div class="banner" >
-							<ul v-for="slide in dataslide" :key="slide.id">
+							<ul>
 								<!-- THE FIRST SLIDE -->
 								<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-									<div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
-										<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" :src="`/src/image/slide/${slide.image}`" data-src="product.image" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('../assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-										</div>
+						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+											<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="./src/image/slide/banner1.jpg" data-src="assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+											</div>
 									</div>
 								</li>
 							</ul>
@@ -33,23 +33,24 @@
 								<p class="pull-left">438 styles found</p>
 								<div class="clearfix"></div>
 							</div>
-
-							<div v-for="product in data":key="product.id" class="row" >
-								<div class="col-sm-3">
-									<div class="single-item">
-										<div class="single-item-header">
-											<a href="product.html"><img :src="`/src/image/product/${product.image}`" alt=""></a>
-										</div>
-										<div class="single-item-body">
-											<p class="single-item-title">{{ product.name }}</p>
-											<p class="single-item-price">
-												<span>${{ product.unit_price }}</span>
-											</p>
-										</div>
-										<div class="single-item-caption">
-											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
-											<div class="clearfix"></div>
+							<div style="display: grid; grid-template-columns: repeat(3, 1fr);">
+								<div v-for="product in data":key="product.id" class="row">
+									<div class="col-sm-10">
+										<div class="single-item">
+											<div class="single-item-header">
+												<a href="product.html"><img :src="`/src/image/product/${product.image}`" alt=""></a>
+											</div>
+											<div class="single-item-body">
+												<p class="single-item-title">{{ product.name }}</p>
+												<p class="single-item-price">
+													<span>${{ product.unit_price }}</span>
+												</p>
+											</div>
+											<div class="single-item-caption">
+												<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+												<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+												<div class="clearfix"></div>
+											</div>
 										</div>
 									</div>
 								</div>
@@ -64,43 +65,47 @@
 								<p class="pull-left">438 styles found</p>
 								<div class="clearfix"></div>
 							</div>
-							<div v-for="product in data" :key="product.id" class="row">
-								<div class="col-sm-3">
-									<div class="single-item">
-										<div class="single-item-header">
-											<a href="product.html"><img :src="product.image" alt=""></a>
-										</div>
-										<div class="single-item-body">
-											<p class="single-item-title">{{ product.name }}</p>
-											<p class="single-item-price">
-												<span>${{ product.unit_price }}</span>
-											</p>
-										</div>
-										<div class="single-item-caption">
-											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
-											<div class="clearfix"></div>
+							<div style="display: grid; grid-template-columns: repeat(3, 1fr);">
+								<div v-for="product in data" :key="product.id" class="row">
+									<div class="col-sm-10">
+										<div class="single-item">
+											<div class="single-item-header">
+												<a href="product.html"><img :src="`/src/image/product/${product.image}`" alt=""></a>
+											</div>
+											<div class="single-item-body">
+												<p class="single-item-title">{{ product.name }}</p>
+												<p class="single-item-price">
+													<span>${{ product.unit_price }}</span>
+												</p>
+											</div>
+											<div class="single-item-caption">
+												<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+												<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+												<div class="clearfix"></div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 							<div class="space40">&nbsp;</div>
-							<div v-for="product in data" :key="product.id" class="row">
-								<div class="col-sm-3">
-									<div class="single-item">
-										<div class="single-item-header">
-											<a href="product.html"><img :src="`/src/image/product/${product.image}`" alt=""></a>
-										</div>
-										<div class="single-item-body">
-											<p class="single-item-title">{{ product.name }}</p>
-											<p class="single-item-price">
-												<span>${{ product.unit_price }}</span>
-											</p>
-										</div>
-										<div class="single-item-caption">
-											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
-											<div class="clearfix"></div>
+							<div style="display: grid; grid-template-columns: repeat(3, 1fr);">
+								<div v-for="product in data" :key="product.id" class="row">
+									<div class="col-sm-10">
+										<div class="single-item">
+											<div class="single-item-header">
+												<a href="product.html"><img :src="`/src/image/product/${product.image}`" alt=""></a>
+											</div>
+											<div class="single-item-body">
+												<p class="single-item-title">{{ product.name }}</p>
+												<p class="single-item-price">
+													<span>${{ product.unit_price }}</span>
+												</p>
+											</div>
+											<div class="single-item-caption">
+												<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+												<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+												<div class="clearfix"></div>
+											</div>
 										</div>
 									</div>
 								</div>
